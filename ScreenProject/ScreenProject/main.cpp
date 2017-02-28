f2a187b0c0ec9632cfccc83be74a047a855a54f5
kwanting97 <kwanting97@hotmail.com>
2017-02-28 13:58:52 -0800

    init project

---
+++ ScreenProject/ScreenProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ScreenProject
+//
+//  Created by Rix Lai on 2/28/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
